c60a81f1a7de6d83cda3f974e9f26c3f0a99b03a
Tanner Hicks <tannerhicks@icloud.com>
2024-08-02 12:22:42 -0400

    final changes

---
+++ challenge-20/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>Tanner Hicks</h1>
+    <nav>
+      <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>About Me</NavLink>
+      <NavLink to="/portfolio" className={({ isActive }) => (isActive ? 'active' : '')}>Portfolio</NavLink>
+      <NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}>Contact</NavLink>
+      <NavLink to="/resume" className={({ isActive }) => (isActive ? 'active' : '')}>Resume</NavLink>
+    </nav>
+  </header>
+);
+
+export default Header;
